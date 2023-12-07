b9e7414d38a6ede3ef16f62c961c51210d0def39
thioaethxr <thioaethxr@proton.me>
2023-12-07 20:31:35 +0100

    Adding and setting up Prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Czech Stack",
-  description: "Czech Stack",
+  title: 'Czech Stack',
+  description: 'Czech Stack',
 };
 
 export default function RootLayout({
